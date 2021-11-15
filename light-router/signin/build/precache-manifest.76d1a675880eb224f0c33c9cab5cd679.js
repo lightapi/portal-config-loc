ecdfcab06ebb6d5b5a5ffc763febaeee21b5df0a
Steve Hu <stevehu@gmail.com>
2021-11-14 23:24:27 -0500

    initial checkin

---
+++ light-router/signin/build/precache-manifest.76d1a675880eb224f0c33c9cab5cd679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6bfb433af2f6be4090c4315c05b52a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9a501cff6524a2ce22",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a3f2714bb7d55bd48a3",
+    "url": "/static/js/2.ec8ee68b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ec8ee68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9a501cff6524a2ce22",
+    "url": "/static/js/main.0bdae82f.chunk.js"
+  },
+  {
+    "revision": "429f41dcc1af5a6a4bca",
+    "url": "/static/js/runtime-main.248612cf.js"
+  }
+]);
